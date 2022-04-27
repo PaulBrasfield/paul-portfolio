c97f48204671b560eb02a3452c741437204fe9d1
Paul <paulbrasfield@hotmail.com>
2022-04-26 19:38:38 -0500

    Changed Contact Page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,12 @@ import Layout from "../components/Layout"
 export default function contact() {
   return (
     <Layout>
-      <div>contact</div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam a optio
+        dicta laboriosam sapiente eum reprehenderit veniam sint alias cupiditate
+        debitis eveniet delectus maiores, labore molestiae aliquid accusantium
+        dolore quaerat?
+      </div>
     </Layout>
   )
 }
